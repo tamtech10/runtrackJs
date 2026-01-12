cf8c9caa5c73c9be323b60d5de374355a3b055cb
tamtech10 <tamtech.dev@gmail.com>
2026-01-12 14:13:43 +0100

    Exo6 code Konami

---
+++ Jour02/Job06/script.js
@@ -0,0 +1,52 @@
+let keys = []; 
+let x = 0;  
+
+let konami = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+];
+
+function styleKonami() {
+    document.body.style.backgroundColor = "#0000FF";
+    document.body.textContent = "La Plateforme";
+    document.body.style.color = "#ffffff";
+}
+
+function codeKonami() {
+
+    let tap = true;
+
+    for (let i = 0; i < 10; i++) {   //If the key is different from the Konami code, the code is incorrect
+        if (keys[i] !== konami[i]) {
+            tap = false;
+        }
+    }
+
+    if (tap) {
+        styleKonami();
+    }
+}
+
+
+window.addEventListener("keydown", function(event) {
+
+    if (x < 10) {                                  //Count pressed keys
+        keys[x] = event.key;
+        x++;
+    }
+
+    codeKonami();
+
+});
+
+
+
+
